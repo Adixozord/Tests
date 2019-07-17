5173b61895920d045c71c1fca9925ded28b15ace
Adrian Falenczyk <falenczyk.adrian@gmail.com>
2019-07-17 21:17:14 +0200

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,31 @@
+package name;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Test2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeOptions options = new ChromeOptions().addArguments("--start-maximized");
+		WebDriver driver = new ChromeDriver(options);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		
+		driver.navigate().to("https://www.staples.co.uk");
+		
+		WebElement search = driver.findElement(By.xpath("//*[@id=\"searchKeywords\"]"));
+		wait.until(ExpectedConditions.visibilityOf(search));
+		search.sendKeys("canon");
+		Thread.sleep(3000);
+		WebElement suggestions = driver.findElement(By.xpath("//*[@id=\"SearchSuggestInner\"]/div/div/div[1]/a[2]"));
+		System.out.println(suggestions.getAttribute("keywordtext"));
+	
+		suggestions.click();
+		
+	}
+}
